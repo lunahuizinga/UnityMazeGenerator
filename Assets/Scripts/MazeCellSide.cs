65a45704e1a27474660aa1ab16b8a1618b177174
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-05-17 18:34:16 +0200

    Added IsSolid variable to MazeCellSide

--- Assets/Scripts/MazeCellSide.cs
+++ Assets/Scripts/MazeCellSide.cs
@@ -1,2 +1,4 @@
-﻿public class MazeCellSide{
+﻿// The class defining the side of a maze cell, defining if it's solid (walled off) or not
+public class MazeCellSide{
+    public bool IsSolid = true;
 }
