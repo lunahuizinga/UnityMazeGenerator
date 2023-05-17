51ac92a48f6def866fe9c2799b96d9287e15e452
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-05-17 18:29:49 +0200

    Added variables and initialisation logic

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -1,2 +1,22 @@
-﻿public class MazeCell{
+﻿// The class representing a single cell of the maze
+public class MazeCell{
+    public MazeCellSide[] CellSides;
+    
+    // Define the amount of sides we want to our cell
+    private const int SideAmount = 4;
+
+    public MazeCell(){
+        // Initialise the array of MazeCellSides
+        CellSides = new MazeCellSide[SideAmount];
+        
+        // Initialise every MazeCellSide in the array
+        for (int i = 0; i < CellSides.Length; i++){
+            CellSides[i] = new MazeCellSide();
+        }
+    }
+
+    // Overload the constructor in case we already have established 
+    public MazeCell(MazeCellSide[] cellSides){
+        CellSides = cellSides;
+    }
 }
--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,5 +1,15 @@
 using UnityEngine;
 public class MazeGenerator : MonoBehaviour{
+    public enum AlgorithmType{
+        RandomDepthFirst
+    }
+    
+    // Sets the size that we want the maze to be
+    public Vector2Int Size;
+    // The maze generation algorithm to use for generating the maze
+    public AlgorithmType Algorithm;
+
+    private MazeCell[,] mazeCells;
 
     // Start is called before the first frame update
     private void Start(){
@@ -7,5 +17,22 @@ public class MazeGenerator : MonoBehaviour{
 
     // Update is called once per frame
     private void Update(){
+    }
+
+    // Generate is used to start a new maze generation cycle
+    private void Generate(){
+    }
+
+    // Initialise initialises the necessary variables to the correct values
+    private void Initialise(){
+        // Initialise the two dimensional cell array with the dimensions set by the user
+        mazeCells = new MazeCell[Size.x, Size.y];
+        
+        // Loop through the array and initialise every cell in the array
+        for (int x = 0; x < mazeCells.GetLength(0); x++)
+        for (int y = 0; y < mazeCells.GetLength(1); y++){
+            MazeCell mazeCell = new();
+            mazeCells[x, y] = mazeCell;
+        }
     }
 }
