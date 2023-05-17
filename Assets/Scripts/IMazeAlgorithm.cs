25f4669753728b76779ee16c0da858d3e7639adb
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-05-17 18:35:17 +0200

    Added Generate method to IMazeAlgorithm

--- Assets/Scripts/IMazeAlgorithm.cs
+++ Assets/Scripts/IMazeAlgorithm.cs
@@ -1,2 +1,5 @@
-﻿public interface IMazeAlgorithm{
+﻿// This interface is meant to be implemented by a class dedicated to a certain maze generation algorithm
+public interface IMazeAlgorithm{
+    // We take the size of the maze to generate and return an array of MazeCells, representing a maze layout
+    public MazeCell[,] Generate(int x, int y);
 }
