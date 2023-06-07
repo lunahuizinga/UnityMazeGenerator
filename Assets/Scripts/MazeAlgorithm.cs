382cfd29c43e4578edf67050d1a991c7d7aa6e9b
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-07 20:59:21 +0200

    Change comment to match the interface to class change

--- Assets/Scripts/MazeAlgorithm.cs
+++ Assets/Scripts/MazeAlgorithm.cs
@@ -1,4 +1,4 @@
-﻿// This interface is meant to be implemented by a class dedicated to a certain maze generation algorithm
+﻿// This abstract class is meant to be implemented by a class dedicated to a certain maze generation algorithm
 public abstract class MazeAlgorithm{
     
     // We take the size of the maze to generate and return an array of MazeCells, representing a maze layout
