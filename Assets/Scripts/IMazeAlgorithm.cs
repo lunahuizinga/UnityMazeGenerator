5478e728cdcf404e7e484a81d6379b19dd97d44e
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-07 16:46:04 +0200

    Clarify method variables

--- Assets/Scripts/IMazeAlgorithm.cs
+++ Assets/Scripts/IMazeAlgorithm.cs
@@ -1,5 +1,5 @@
 ﻿// This interface is meant to be implemented by a class dedicated to a certain maze generation algorithm
 public interface IMazeAlgorithm{
     // We take the size of the maze to generate and return an array of MazeCells, representing a maze layout
-    public MazeCell[,] Generate(int x, int y);
+    public MazeCell[,] Generate(int sizeX, int sizeY);
 }
