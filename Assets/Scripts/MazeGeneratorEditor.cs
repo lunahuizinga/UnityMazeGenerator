50eb9e5052639e85f3b387730e2eec7ab859b1a3
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-07 17:09:06 +0200

    Add generate button to custom generator inspector

--- Assets/Scripts/MazeGeneratorEditor.cs
+++ Assets/Scripts/MazeGeneratorEditor.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEditor;
+using UnityEngine;
+
 [CustomEditor(typeof(MazeGenerator))]
 public class MazeGeneratorEditor : Editor{
     public override void OnInspectorGUI(){
+        MazeGenerator generator = (MazeGenerator) target;
         DrawDefaultInspector();
+        if (GUILayout.Button("Generate")) generator.Generate();
     }
 }
