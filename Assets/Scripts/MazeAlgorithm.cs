007966527a8afae90f13ce849d7481031fed6b4e
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-07 21:02:12 +0200

    Add comment with further clarification

--- Assets/Scripts/MazeAlgorithm.cs
+++ Assets/Scripts/MazeAlgorithm.cs
@@ -2,5 +2,6 @@
 public abstract class MazeAlgorithm{
     
     // We take the size of the maze to generate and return an array of MazeCells, representing a maze layout
+    // The method used is up to the individual implementation of this class
     public abstract MazeCell[,] Generate(int sizeX, int sizeY);
 }
