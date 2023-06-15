493e7dadaba544403ed95b362cf99042544f8850
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-15 22:44:55 +0200

    Instantiate proper prefabs and call generation logic

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -26,6 +26,9 @@ public class MazeGenerator : MonoBehaviour{
 
     // Generate is used to start a new maze generation cycle
     public void Generate(){
+        Initialise();
+        MazeCell[,] generatedMaze = algorithmInstance.Generate(Size.x, Size.y);
+        BuildMaze(generatedMaze);
     }
 
     // Initialise initialises the necessary variables to the correct values
@@ -34,5 +37,16 @@ public class MazeGenerator : MonoBehaviour{
             AlgorithmType.RandomDepthFirst => new RandomDepthFirst(),
             _ => throw new ArgumentOutOfRangeException()
         };
+    }
+
+    // Loop through all cells in the array and build the maze
+    private void BuildMaze(MazeCell[,] mazeCells){
+        GameObject maze = new("Maze");
+        foreach (MazeCell mazeCell in mazeCells){
+            Vector3 position = new(mazeCell.X, 0, mazeCell.Y);
+            GameObject cellObject = Instantiate(CellPrefab, position, Quaternion.Euler(Vector3.zero), maze.transform);
+            MazeCellBuilder mazeCellBuilder = cellObject.GetComponent<MazeCellBuilder>();
+            mazeCellBuilder.BuildCell(mazeCell);
+        }
     }
 }
