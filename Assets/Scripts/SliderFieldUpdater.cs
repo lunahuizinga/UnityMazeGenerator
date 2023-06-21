a8d83c96a61a8177cf7cded2209df547f3e20ab6
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2023-06-22 00:02:35 +0200

    Add comments to SliderFieldUpdater

--- Assets/Scripts/SliderFieldUpdater.cs
+++ Assets/Scripts/SliderFieldUpdater.cs
@@ -2,12 +2,16 @@ using TMPro;
 using UnityEngine;
 public class SliderFieldUpdater : MonoBehaviour{
     
+    // Our instance of TextMeshProUGUI, representing a UI label
     public TextMeshProUGUI TextMeshProInstance;
-
+    
+    // If TextMeshProInstance isn't set, get the local instance
     private void Reset(){
         if (TextMeshProInstance == null) TextMeshProInstance = GetComponent<TextMeshProUGUI>();
     }
-
+    
+    // This method simple assigns the passed in float newValue to the text field.
+    // We set the slider in the inspector so that we call this method whenever the slider is changed.
     public void OnValueChanged(float newValue){
         TextMeshProInstance.text = newValue.ToString();
     }
